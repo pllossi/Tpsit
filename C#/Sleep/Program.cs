eccdebe4af31adb2e5b9d6c274c3ddbfab8963cb
pllossi <filippo.boschi.008@gmail.com>
2025-10-09 21:52:07 +0200

    Progetto Sleep in C# e aggiunta di cartelle per linguaggio

---
+++ C#/Sleep/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sleeping for 5 seconds...");
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine("Awake now!");
+        }
+    }
+}
