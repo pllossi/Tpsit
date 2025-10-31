61c9e5391b43420ab3992657fe6bf84f52138ed6
pllossi <115420435+pllossi@users.noreply.github.com>
2025-10-31 11:55:55 +0100

    Es thread

--- C#/EsThread/EsThread/Program.cs
+++ C#/EsThread/EsThread/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+
+namespace EsThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Test.Method);
+            Console.WriteLine("Starting thread...");
+            t.Start();
+            Console.WriteLine(t.ThreadState);
+            Thread.Sleep(30000); 
+            Console.WriteLine("Main thread ending.");
+            t.Join();
+        }
+    }
+
+    static class Test
+    {
+        public static void Method()
+        {
+            Console.WriteLine("Test Method");
+        }
+    }
+}
